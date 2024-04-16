284bf5ea03ff85465b724c58e5dc2e314a81e0ac
maxpang93 <maxpang93@gmail.com>
2024-04-16 22:20:37 +0800

    fix: properly pass onChange func to TextSquare

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import './style.css';
 
-import { useState} from 'react';
+import { useState } from 'react';
 
 function App() {
   return (
@@ -12,17 +12,16 @@ function App() {
   );
 }
 
-function TextSquare({value, isInputDisabled, onCellChange}) {
-  console.log(typeof(onCellChange))
+function TextSquare({ value, isInputDisabled, onCellChange }) {
   return (
     <>
-    <input 
-      className="text-square"
-      type="text"
-      value={value}
-      maxLength={1}
-      onChange={onCellChange}
-      disabled={isInputDisabled}
+      <input
+        className="text-square"
+        type="text"
+        value={value}
+        maxLength={1}
+        onChange={onCellChange}
+        disabled={isInputDisabled}
       />
     </>
   )
@@ -30,29 +29,29 @@ function TextSquare({value, isInputDisabled, onCellChange}) {
 
 function Game() {
   let matrix = [
-    [1,2,3, , , 6, 7, 8, 9],
-    [1,2,3, , , 6, 7, 8, 9], 
-    [1,2,3, , , 6, 7, 8, 9],
-    [1,2,3, , , 6, 7, 8, 9],
-    [1,2,3, , , 6, 7, 8, 9],
-    [1,2,3, , , 6, 7, 8, 9],
+    [1, 2, 3, , , 6, 7, 8, 9],
+    [1, 2, 3, , , 6, 7, 8, 9],
+    [1, 2, 3, , , 6, 7, 8, 9],
+    [1, 2, 3, , , 6, 7, 8, 9],
+    [1, 2, 3, , , 6, 7, 8, 9],
+    [1, 2, 3, , , 6, 7, 8, 9],
 
-    [1,2,3, , , 6, 7, 8, 9],
-    [1,2,3, , , 6, 7, 8, 9],
-    [1,2,3, , , 6, 7, 8, 9],
+    [1, 2, 3, , , 6, 7, 8, 9],
+    [1, 2, 3, , , 6, 7, 8, 9],
+    [1, 2, 3, , , 6, 7, 8, 9],
 
 
   ]
   return (
     <div className="game-container">
       <div>
-        <Grid matrix={matrix}/>
+        <Grid matrix={matrix} />
       </div>
     </div>
   )
 }
 
-function Grid({matrix}) {
+function Grid({ matrix }) {
   const [gridValues, setGridValues] = useState(Array(9).fill(Array(9).fill("")))
 
   const [gridCellsDisabled, setGridCellsDisabled] = useState(Array(9).fill(Array(9).fill(false)))
@@ -60,13 +59,21 @@ function Grid({matrix}) {
 
   // setGridValues(matrix)
 
-  async function handleChangeFnGenerator(i, j) {
+  // disable cells that are initially filled
+  const cellsDisabled = deepcopy(matrix)
+  for (let i = 0; i < cellsDisabled.length; i++) {
+    for (let j = 0; j < cellsDisabled[0].length; j++) {
+      cellsDisabled[i][j] = !!cellsDisabled[i][j]
+    }
+  }
+
+  async function handleChange(i, j, event) {
     // regex
     const isNumeric = (string) => /^[+-]?\d+(\.\d+)?$/.test(string)
 
     // validate if numeric and valid sudoku move
     const validateInput = async (inputValue) => {
-      console.log("type: ", typeof(inputValue))
+      console.log("type: ", typeof (inputValue))
       console.log("inputValue: ", inputValue)
       // validate must be 1-9
       if (!isNumeric(inputValue)) {
@@ -77,29 +84,28 @@ function Grid({matrix}) {
       return Math.random() > 0.5 // TO remove
     }
 
-    async function handleChange(event) {
-      const inputValue = event.target.value
-      if (inputValue.length === 0) {
-        console.log("inputValue is empty")
-        return
-      }
-      gridCellsDisabled[i][j] = false
-      setGridCellsDisabled(gridCellsDisabled)
 
-      const valid = await validateInput(inputValue)
-      console.log("valid: ", valid)
-
-      if (valid) {
-        gridValues[i][j] = inputValue
-        setGridValues(gridValues)
-      } else {
-        console.error("Input is invalid. Reseting value and enable square.")
-        gridCellsDisabled[i][j] = true
-        setGridCellsDisabled(gridCellsDisabled)
-      }
+    const inputValue = event.target.value
+    if (inputValue.length === 0) {
+      console.log("inputValue is empty")
+      return
     }
-    return handleChange
+    gridCellsDisabled[i][j] = false
+    setGridCellsDisabled(gridCellsDisabled)
+
+    const valid = await validateInput(inputValue)
+    console.log("valid: ", valid)
+
+    if (valid) {
+      gridValues[i][j] = inputValue
+      setGridValues(gridValues)
+    } else {
+      console.error("Input is invalid. Reseting value and enable square.")
+      gridCellsDisabled[i][j] = true
+      setGridCellsDisabled(gridCellsDisabled)
+    }
   }
+
   return (
     <>
       {Array(9).fill(null).map((_, i) => (
@@ -107,13 +113,13 @@ function Grid({matrix}) {
           {Array(9).fill(null).map((_, j) => {
             return (
               <TextSquare
-                key={`${i}${j}`} 
-                value={gridValues[i][j]}
-                isInputDisabled={gridCellsDisabled[i][j]}
-                onCellChange={handleChangeFnGenerator(i,j)}
+                key={`${i}${j}`}
+                value={matrix[i][j]}
+                isInputDisabled={cellsDisabled[i][j]}
+                onCellChange={(event) => handleChange(i, j, event)}
               />
-              );
-            })}
+            );
+          })}
         </div>
       ))}
     </>
@@ -127,5 +133,7 @@ function Square({ value, onSquareClick }) {
     </button>
   )
 }
+
+const deepcopy = (someObject) => JSON.parse(JSON.stringify(someObject))
 
 export default App;
