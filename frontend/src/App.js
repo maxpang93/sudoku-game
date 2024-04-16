fa50539cd88b9c27da36e1b10cbbb154dbd40263
maxpang93 <maxpang93@gmail.com>
2024-04-16 20:11:31 +0800

    add: Draft Grid and TextSquare component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,144 @@
-import logo from './logo.svg';
 import './App.css';
+import './style.css';
+
+import { useState} from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Game />
+      <TextSquare />
     </div>
   );
+}
+
+function TextSquare() {
+  const [value, setValue] = useState("")
+  const [isInputDisabled, setIsInputDisabled] = useState(false)
+
+  const handleChange = async (event) => {
+    console.log("evnt", event)
+
+    const inputValue = event.target.value
+    if (inputValue.length === 0) {
+      console.log("inputValue is empty")
+      return
+    }
+  
+    setIsInputDisabled(true)
+    
+    const valid = await validateInput(inputValue)
+    console.log("valid: ", valid)
+    if (valid) {
+      setValue(inputValue)
+    } else {
+      console.error("Input is invalid. Reseting value and enable square.")
+      setValue("")
+      setIsInputDisabled(false)
+    }
+  }
+
+  const isNumeric = (string) => /^[+-]?\d+(\.\d+)?$/.test(string)
+
+  const validateInput = async (inputValue) => {
+    console.log("type: ", typeof(inputValue))
+    console.log("inputValue: ", inputValue)
+    // validate must be 1-9
+    if (!isNumeric(inputValue)) {
+      console.warn("Input is NOT a number.")
+      return false
+    }
+
+    return Math.random() > 0.5 // TO remove
+
+    // call backend to validate if input fulfills sudoku rules
+    try {
+      // TODO:
+      const resp = await fetch("https://httpbin.org/post", {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({input: inputValue})
+      })
+
+      const data = await resp.json()
+      console.log(data)
+
+    } catch (err) {
+      console.error(`Failed to validate input: ${err}`)
+      return false
+    }
+
+    return true
+  }
+
+  return (
+    <>
+    <input 
+      className="text-square"
+      type="text"
+      value={value}
+      maxLength={1}
+      onChange={handleChange}
+      disabled={isInputDisabled}
+      />
+    </>
+  )
+}
+
+function Game() {
+  let matrix = [
+    [1,2,3, , , 6, 7, 8, 9],
+    [1,2,3, , , 6, 7, 8, 9], 
+    [1,2,3, , , 6, 7, 8, 9],
+    [1,2,3, , , 6, 7, 8, 9],
+    [1,2,3, , , 6, 7, 8, 9],
+    [1,2,3, , , 6, 7, 8, 9],
+
+    [1,2,3, , , 6, 7, 8, 9],
+    [1,2,3, , , 6, 7, 8, 9],
+    [1,2,3, , , 6, 7, 8, 9],
+
+
+  ]
+  return (
+    <div className="game-container">
+      <div>
+        <Grid matrix={matrix}/>
+      </div>
+    </div>
+  )
+}
+
+function Grid({matrix}) {
+  const onSquareClick = () => { console.log("this is onSquareClick") }
+
+  return (
+    <>
+      {Array(9).fill(null).map((_, i) => (
+        <div className="grid-row" key={i}>
+          {Array(9).fill(null).map((_, j) => {
+            return (
+              <Square
+                key={`${i}${j}`} 
+                value={matrix[i][j]}
+                onSquareClick={onSquareClick}
+              />
+              );
+            })}
+        </div>
+      ))}
+    </>
+  )
+}
+
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  )
 }
 
 export default App;
